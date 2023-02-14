10390f4aea948c4415205a5687cd5c5907c503e7
unknown <hamayoon.47611@iqra.edu.pk>
2023-02-14 18:27:42 +0500

    toopak live

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="title">
+        {" "}
+        <h1>Toopak</h1>
+        <h3>Sports</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
